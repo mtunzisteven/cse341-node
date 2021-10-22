0657699b7561b9fc2809aea6e1cf26931de79bf9
mtunzisteven <mtunzism@byui.edu>
2021-10-22 09:44:12 +0200

    two options not supported | commented them out

--- index.js
+++ index.js
@@ -101,8 +101,8 @@ app.use(cors(corsOptions));
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false, 
+    // useCreateIndex: true,
+    // useFindAndModify: false, 
     family: 4
 };
 
